53d69e6b6d79a3d5ea186d72434b5c1c478d2c59
Davide <davide.merassi@studenti.unimi.it>
2024-04-13 17:09:54 +0200

    start

---
+++ app/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.example.sandboxtest"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.sandboxtest"
+        minSdk = 23
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment:2.7.7")
+    implementation("androidx.navigation:navigation-ui:2.7.7")
+    implementation("com.tencent:mmkv-static:1.2.10")
+    implementation ("com.google.code.gson:gson:2.9.1")
+    implementation ("org.jetbrains.kotlin:kotlin-stdlib:1.5.30")
+    implementation(files("libs/SpaceCore-SDK.aar"))
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
---
+++ app/src/main/java/com/example/sandboxtest/MyApplication.java
@@ -0,0 +1,31 @@
+package com.example.sandboxtest;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.fvbox.lib.FCore;
+
+public class MyApplication extends Application {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+
+        FCore.get().init(this);
+        FCore.get().setAllowSystemInteraction(true);
+        FCore.get().setAutoPreloadApplication(true);
+        if(FCore.isClient()) {
+            return;
+        }
+        // do something...
+        // Add the code you want to execute during initialization here
+        // For example:
+        // MyInitializationClass.init(base);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Other initialization code that you want to execute when the application is created
+    }
+}
